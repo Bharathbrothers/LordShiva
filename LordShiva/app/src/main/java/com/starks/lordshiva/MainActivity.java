7a487963bed17fb407f6f755b97dda345c40bcbb
Bharath <sathuribharathbrothers1234@gmail.com>
2018-01-31 05:29:44 +0530

    Files

---
+++ LordShiva/app/src/main/java/com/starks/lordshiva/MainActivity.java
@@ -0,0 +1,25 @@
+package com.starks.lordshiva;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SlideActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
